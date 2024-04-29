22631dc1f8ecc71ea30a464d6ae717ad27c39964
jingon0 <168407337+jingon0@users.noreply.github.com>
2024-04-29 16:42:57 +0900

    Add files via upload

---
+++ week2/ConsoleApp1/ReadData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csbemt
+{
+    public class ReadData
+    {
+        public void Print_dat(string file_path)
+        {
+            try
+            {
+                // FileStream을 사용하여 파일 열기
+                using (FileStream fs = new FileStream(file_path, FileMode.Open))
+                {
+                    // StreamReader를 사용하여 파일 읽기
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        string line;
+
+                        while ((line = sr.ReadLine()) != null)
+                            Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // 오류 처리
+                Console.WriteLine("파일을 읽는 도중 오류가 발생했습니다: " + ex.Message);
+            }
+       }
+
+        static string ConsolidateSpaces(string input)
+        {
+            // 공백을 기준으로 문자열 분할
+            string[] parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            // 분할된 부분을 다시 합침
+            return string.Join(" ", parts);
+        }
+    }
+}
---
+++ week2/ConsoleApp1/test.cs
@@ -0,0 +1,19 @@
+﻿using csbemt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class test
+    {
+        static void Main(string[] args)
+        { 
+            ReadData Read = new ReadData();
+
+            Read.Print_dat("C:\\Users\\imsuh\\OneDrive - 한서대학교\\2_서류\\1_한서대학교\\4_한서튜터링\\3_24년도_1학기_C가쉽다면믿으시겠습니까\\2주차\\ConsoleApp1\\abc.dat");
+        }
+    }
+}
